16915373ccb4d841e996cff52cf4c32952c656db
stanleeniko <stanleeniko@gmail.com>
2019-08-17 02:07:06 +0530

    refinements

--- website/create.js
+++ website/create.js
@@ -8,20 +8,99 @@ var url2 = ser2 + imagelink;
 var sel;
 var myClass;
 var currSel;
-var imagelink;
+var imglink;
 
 console.log(url);
 var gData;
+//////
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      $(".image-upload-wrap").hide();
+
+      $(".file-upload-image").attr("src", e.target.result);
+      $(".file-upload-content").show();
+
+      $(".image-title").html(input.files[0].name);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+
+    var $files = $(input).get(0).files;
+
+    if ($files.length) {
+      // Reject big files
+      if ($files[0].size > $(this).data("max-size") * 1024) {
+        console.log("Please select a smaller file");
+        return false;
+      }
+
+      // Begin file upload
+      console.log("Uploading file to Imgur..");
+
+      // Replace ctrlq with your own API key
+      var apiUrl = "https://api.imgur.com/3/image";
+      var apiKey = "e48ab4fb905ea1b";
+
+      var settings = {
+        async: false,
+        crossDomain: true,
+        processData: false,
+        contentType: false,
+        type: "POST",
+        url: apiUrl,
+        headers: {
+          Authorization: "Client-ID " + apiKey,
+          Accept: "application/json"
+        },
+        mimeType: "multipart/form-data"
+      };
+
+      var formData = new FormData();
+      formData.append("image", $files[0]);
+      settings.data = formData;
+
+      // Response contains stringified JSON
+      // Image URL available at response.data.link
+      $.ajax(settings).done(function(response) {
+        console.log(response);
+        //var newData = JSON.parse(data).pri_tag;
+        imglink = JSON.parse(response).data.link;
+
+        console.log(imglink);
+      });
+    }
+  } else {
+    removeUpload();
+  }
+}
+
+function removeUpload() {
+  $(".file-upload-input").replaceWith($(".file-upload-input").clone());
+  $(".file-upload-content").hide();
+  $(".image-upload-wrap").show();
+}
+$(".image-upload-wrap").bind("dragover", function() {
+  $(".image-upload-wrap").addClass("image-dropping");
+});
+$(".image-upload-wrap").bind("dragleave", function() {
+  $(".image-upload-wrap").removeClass("image-dropping");
+});
+////////////
 
 $.getJSON(url, function(data) {
   gData = data;
   console.log(data);
   var text;
   for (i = 0; i < data.length; i++) {
-    text += `<li>Dynamic
-              ${data[i].type}
+    text += `<li>Component &nbsp
+              ${data[i].type}&nbsp ID:
               ${data[i].id}
               <button class="${data[i].type}" id="${data[i].id}">select</button>
+              <br/>
+              <br/>
               </li>`;
   }
   $("#ShowImage").attr("src", url2);
@@ -239,7 +318,7 @@ $("#submit").click(function() {
         console.log(`Data entered: height-------->` + gData[i].height);
         gData[i].width = $("#img_wt").val();
         console.log(`Data entered: width-------->` + gData[i].width);
-        gData[i].imgurl = imagelink; //$("#img_url").val();
+        gData[i].imgurl = imglink; //$("#img_url").val();
         console.log(`Data entered: imgurl-------->` + gData[i].imgurl);
       }
     }
@@ -248,80 +327,4 @@ $("#submit").click(function() {
   data = JSON.stringify(gData);
   console.log(data);
   localStorage.setItem("gdata", data);
-});
-
-function readURL(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-
-    reader.onload = function(e) {
-      $(".image-upload-wrap").hide();
-
-      $(".file-upload-image").attr("src", e.target.result);
-      $(".file-upload-content").show();
-
-      $(".image-title").html(input.files[0].name);
-    };
-
-    reader.readAsDataURL(input.files[0]);
-
-    var $files = $(input).get(0).files;
-
-    if ($files.length) {
-      // Reject big files
-      if ($files[0].size > $(this).data("max-size") * 1024) {
-        console.log("Please select a smaller file");
-        return false;
-      }
-
-      // Begin file upload
-      console.log("Uploading file to Imgur..");
-
-      // Replace ctrlq with your own API key
-      var apiUrl = "https://api.imgur.com/3/image";
-      var apiKey = "e48ab4fb905ea1b";
-
-      var settings = {
-        async: false,
-        crossDomain: true,
-        processData: false,
-        contentType: false,
-        type: "POST",
-        url: apiUrl,
-        headers: {
-          Authorization: "Client-ID " + apiKey,
-          Accept: "application/json"
-        },
-        mimeType: "multipart/form-data"
-      };
-
-      var formData = new FormData();
-      formData.append("image", $files[0]);
-      settings.data = formData;
-
-      // Response contains stringified JSON
-      // Image URL available at response.data.link
-      $.ajax(settings).done(function(response) {
-        console.log(response);
-        //var newData = JSON.parse(data).pri_tag;
-        var imagelink = JSON.parse(response).data.link;
-
-        console.log(JSON.parse(response).data.link);
-      });
-    }
-  } else {
-    removeUpload();
-  }
-}
-
-function removeUpload() {
-  $(".file-upload-input").replaceWith($(".file-upload-input").clone());
-  $(".file-upload-content").hide();
-  $(".image-upload-wrap").show();
-}
-$(".image-upload-wrap").bind("dragover", function() {
-  $(".image-upload-wrap").addClass("image-dropping");
-});
-$(".image-upload-wrap").bind("dragleave", function() {
-  $(".image-upload-wrap").removeClass("image-dropping");
 });

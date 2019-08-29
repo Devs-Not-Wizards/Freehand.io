98469a83fc3cfd1e33178197759c5a59c552a64f
stanleeniko <stanleeniko@gmail.com>
2019-08-30 01:58:16 +0530

    codepage with editor

--- website/new_generate.js
+++ website/new_generate.js
@@ -1,12 +1,25 @@
 var data = localStorage.getItem("gdata");
-
+var code;
 console.log(data);
 
 $.get("https://softechlab.herokuapp.com/code?data=" + data, function(data) {
   console.log(data);
   $(".mypanel").html(data);
+  code = data;
+  var x = data.toString();
+
+  var res = x.replace(/<plaintext>/g, "");
+  console.log(res);
+  var r = '"' + res.toString() + '"';
+  $("#code").text(res);
 });
 
 var url = "https://softechlab.herokuapp.com/show?data=" + data;
 console.log(url);
 $("#frame").attr("src", url);
+$(document).ready(function() {
+  $("#code").click(function() {
+    alert("This paragraph was clicked.");
+    $("#frame").html(code);
+  });
+});

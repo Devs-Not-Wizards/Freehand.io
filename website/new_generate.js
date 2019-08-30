dfbea947a7c132e7c01654dbcde71f2b75df2239
stanleeniko <stanleeniko@gmail.com>
2019-08-30 22:35:58 +0530

    codepage + editor page

--- website/new_generate.js
+++ website/new_generate.js
@@ -23,3 +23,15 @@ $(document).ready(function() {
   //   $("#frame").html(code);
   // });
 });
+
+/**
+ *
+ * In this pen:
+ * added scripts with CDN:
+ *   -  //cdnjs.cloudflare.com/ajax/libs/codemirror/5.13.4/codemirror.js
+ *   -  //cdnjs.cloudflare.com/ajax/libs/codemirror/5.13.4/mode/xml/xml.js
+ *   -  //cdnjs.cloudflare.com/ajax/libs/codemirror/5.13.4/mode/css/css.js
+ *   -  //cdnjs.cloudflare.com/ajax/libs/codemirror/5.13.4/mode/javascript/javascript.js
+ *   -  //cdnjs.cloudflare.com/ajax/libs/codemirror/5.13.4/mode/htmlmixed/htmlmixed.js
+ *
+ */
